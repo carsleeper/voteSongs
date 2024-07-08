8434897db5a9cd7c6553b88a8584f9d285e5dd7e
Hyunwoo <hw1115.lee@gmail.com>
2024-07-08 09:50:47 +0900

    change GUI framework from Tkinter to PyQt5

--- source/song.py
+++ source/song.py
@@ -1,104 +1,130 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+import sys
 import csv
-from tkinter import filedialog
-from tkinter import *
-from tkinter import messagebox
+## 구상 : 설정창에서 기타세션 최소 충족 인원 수 등등을 정할 수 있게 하고,
+##        결과창에서 가능여부, 가능하면 그 곡의 맴버들을 정해줌. 
 
-filedir = ""
-songDictionary = {}
+class voteSongs(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
 
-#frame change
-def openFrame(frame):
-    frame.tkraise()
-    
-#loading csv file
-def fileSelection():
-    global filedir
-    filePath = filedialog.askopenfilename(title="파일을 선택하세요",filetypes=[("csv files", "*.csv")])
-    if filePath:
-        global numberOfPeople
-        filedir = f"{filePath}"
-        openFrame(mainFrame)
-        with open(filedir, 'r',encoding="utf-8") as w:
+    def initUI(self):
+        self.setWindowTitle('voteSongs')
+        self.resize(700,400)
+        #open window at center
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+        self.windowPage = QStackedWidget()
+        self.loadFile()
+        self.songDictionary = {}
+
+        self.show()
+
+    def addWindowPage(self, Groupbox):
+        self.windowPage.addWidget(Groupbox)
+        self.setCentralWidget(self.windowPage)
+
+    def selectWindowPage(self, QWid):
+        self.windowPage.setCurrentWidget(QWid)
+
+    def sorting(self):
+        #defining numberOfPeople
+        with open(self.fname[0], 'r',encoding="utf-8") as w:
             lines = w.readlines()
-            numberOfPeople = len(lines)-1
-            createLabels()
-    else:
-        messagebox.showwarning("오류", "파일을 선택하세요.")
+            self.numberOfPeople = len(lines)-1
+        
+        sortingGroupBox = QGroupBox()
+        sortingHbox = QHBoxLayout()
+        sortingVbox = QVBoxLayout()
+        sortingHbox.addStretch(1)
+        sortingHbox.addLayout(sortingVbox)
+        sortingHbox.addStretch(1)
 
-def createLabels():
-    global label0
-    for i in range(0,((numberOfPeople)//2)+1):
-        if i == 0:
-            label0 = Label(mainFrame, text="모두가 동의 한 곡:",font=("Arial", 20))
-            label0.place(relx=0.5,rely=0, anchor="n")
-        else:
-            globals()[f"label{i}"] = Label(mainFrame,text=f"{i}명 빼고 동의한 곡:", font=("Arial", 20))
-            globals()[f"label{i}"].place(relx=0.5,rely=i/(((numberOfPeople)//2)+2), anchor="n")
-    mainFrame.update_idletasks()
-    sorting()
+        sortingGroupBox.setLayout(sortingHbox)
 
+        self.addWindowPage(sortingGroupBox)
+        self.selectWindowPage(sortingGroupBox)
 
-def sorting():
-    global songDictionary
-    # CSV 파일을 열고 DictReader를 사용하여 읽기
-    with open(filedir, newline='') as work:
-        content = csv.DictReader(work)
+        self.bigfont = QFont()
+        self.bigfont.setPointSizeF(20)
+
+        self.smallfont = QFont()
+        self.smallfont.setPointSizeF(15)
+
+        #make informing label 
+        for i in range(0,((self.numberOfPeople)//2)+1):
+            if i == 0:
+                self.label0 = QLabel('모두가 동의 한 곡', self)
+            else:
+                setattr(self, f"label{i}", QLabel(f'{i}명 빼고 동의한 곡'))
+
         #songDictionary Emptying
-        for songs in content:
-            for i, song in enumerate(songs.keys()):
-                if i == 0 or i == 1:
-                    continue
-                songDictionary[song] = list()
+        with open(self.fname[0],'r', newline='', encoding='utf-8') as work:
+            content = csv.DictReader(work)
+            for songs in content:
+                for i, song in enumerate(songs.keys()):
+                    if i == 0 or i == 1:
+                        continue
+                    self.songDictionary[song] = list()
 
-    with open(filedir, newline='') as work:
-        content = csv.DictReader(work)
         #songDictionary making
-        for songs in content:
-            for i, song in enumerate(songs.keys()):
-                if i == 0 or i == 1:
-                    continue
-                songDictionary[song].append(songs[song])
+        with open(self.fname[0],'r', newline='', encoding='utf-8') as work:
+            content = csv.DictReader(work)
+            for songs in content:
+                for i, song in enumerate(songs.keys()):
+                    if i == 0 or i == 1:
+                        continue
+                    self.songDictionary[song].append(songs[song])
 
-        #agreeList making
-        for i in range(0,(numberOfPeople//2)+1):
-            globals()[f"agree{i}List"] = list()
-            for song in songDictionary.keys():
-                if songDictionary[song].count("good") == numberOfPeople-i:
-                    globals()[f"agree{i}List"].append(song)
-            
+            #agreeList making
+            for i in range(0,(self.numberOfPeople//2)+1):
+                setattr(self, f'agreeList{i}', list())
+                for song in self.songDictionary.keys():
+                    if self.songDictionary[song].count("good") == self.numberOfPeople-i:
+                        getattr(self, f'agreeList{i}').append(song)
 
-        #label attaching
-        for j in range(0,(numberOfPeople//2)+1):
-            for i, song in enumerate(globals()[f"agree{j}List"], start=1):
-                    label = Label(mainFrame, text = song, font=("Arial", 13))
-                    if j == 0:
-                        label.place(relx=0.5,rely= (label0.winfo_y()+(label1.winfo_y() - label0.winfo_y())*(i/(len(globals()[f"agree{j}List"])+1))) / mainFrame.winfo_height(), anchor="n")
-                    elif j == (numberOfPeople//2):
-                        label.place(relx=0.5,rely=(globals()[f"label{j}"].winfo_y()+float((1 - (mainFrame.winfo_height())*globals()[f"label{j}"].winfo_y())*(i/(len(globals()[f"agree{j}List"])+1)))) / mainFrame.winfo_height(), anchor="n")
-                    else:
-                        label.place(relx=0.5,rely=(globals()[f"label{j}"].winfo_y()+float((globals()[f"label{j+1}"].winfo_y() - globals()[f"label{j}"].winfo_y())*(i/(len(globals()[f"agree{j}List"])+1)))) / mainFrame.winfo_height(), anchor="n")
+            #Labelling
+            for i in range(0,(self.numberOfPeople//2)+1):
+                getattr(self,f"label{i}").setFont(self.bigfont)
+                sortingVbox.addWidget(getattr(self,f"label{i}"))
+                for song in getattr(self,f'agreeList{i}'):
+                    label = QLabel(song)
+                    label.setFont(self.smallfont)
+                    sortingVbox.addWidget(label)
+                sortingVbox.addSpacing(25)
+
+    def showFileDialog(self):
+        while True:
+            self.fname = QFileDialog.getOpenFileName(self,'Open csv file','./','csv Files(*.csv)')
+            if self.fname[0]:
+                break
+            else:
+                QMessageBox.question(self, 'Error','please choose file.', QMessageBox.Yes, QMessageBox.Yes)
+        self.sorting()
+
+    # loadFileBox < loadFileGroupbox < windowPage
+    def loadFile(self):
+        loadFileGroupbox = QGroupBox()
+        informBtn = QPushButton('Click to load .csv file')
+        informBtn.clicked.connect(self.showFileDialog)
+        loadFileVbox = QVBoxLayout()
+        loadFileVbox.addWidget(informBtn)
+        loadFileHbox = QHBoxLayout()
+        loadFileHbox.addStretch(1)
+        loadFileHbox.addLayout(loadFileVbox)
+        loadFileHbox.addStretch(1)
+
+        loadFileGroupbox.setLayout(loadFileHbox)
+
+        self.addWindowPage(loadFileGroupbox)
+        self.selectWindowPage(loadFileGroupbox)
 
 
-#fundametal settings
-window = Tk()
-window.title("선곡")
-window.geometry("1200x700")
-
-#main frame
-mainFrame = Frame(window)
-mainFrame.place(x=0,y=0, relheight=1,relwidth=1)
-
-
-
-#csv selection frame
-previousFrame = Frame(window)
-previousFrame.place(x=0,y=0, relheight=1,relwidth=1)
-inform = Label(previousFrame, text="csv파일을 선택하세요", font=("Arial", 20))
-inform.place(relx=0.5,rely=0.4,anchor="center")
-csvSeletion = Button(previousFrame,text="선택", command= fileSelection)
-csvSeletion.place(relx=0.5,rely=0.5,anchor="center")
-
-openFrame(previousFrame)
-window.mainloop()
-
-
+if __name__ =="__main__":
+    app =QApplication(sys.argv)
+    ex = voteSongs()
+    sys.exit(app.exec_())
